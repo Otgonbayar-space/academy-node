74fc6ffc3d58f7db878fe3842c634a6df478a1c0
BACK-14 <back-14@Erxess-Mac-mini.local>
2025-11-04 18:25:29 +0800

    node iig jargaav

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+import { readFileSync } from 'fs';
 
-import * as fs from "node:fs";
-
-const a = new Promise(resolve  => {
-    resolve(`12341234`);
-
-})
-
-
-a.then(data => {
-    console.log(data)
-
-});
+export function readUsers() {
+  try {
+    const data = readFileSync('users.txt', 'utf-8');
+    const users = data
+      .split('\n')
+      .map(line => line.trim())
+      .filter(line => line !== '');
+    return users;
+  } catch (err) {
+    console.error('Файл уншихад алдаа гарлаа:', err);
+    return [];
+  }
+}
