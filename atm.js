cefac7f3691474e70fb757347720ade42a39002b
BACK-14 <back-14@Erxess-Mac-mini.local>
2025-11-06 20:26:53 +0800

    idk

--- atm.js
+++ atm.js
@@ -10,128 +10,111 @@ const rl = readline.createInterface({
 
 // readUsers(): users.txt-ээс унших
 
-
 function readUsers() {
-    if(!fs.existsSync("users.txt")) return []
-
+  if (!fs.existsSync("users.json")) return [];
 
   // 👉 Хэрэглэгчийн мэдээллийг унших код
-  const data = fs.readFileSync("users.txt", 'utf-8');
-  if(!data.trim()) return [];
+  const data = fs.readFileSync("users.json", "utf-8");
+  if (!data.trim()) return [];
 
-    return data.split("\n").map((line) => {
-        const [username, pin, balance] = line.split(",")
-        return {username :username.trim(), pin:pin.trim(), balance:parseInt(balance.trim())};
-
-    })
-    
+  return data.split("\n").map((line) => {
+    const [username, pin, balance] = line.split(",");
+    return {
+      username: username.trim(),
+      pin: pin.trim(),
+      balance: parseInt(balance.trim()),
+    };
+  });
 }
 
 // writeUsers(): users.txt-д бичих
 function writeUsers(users) {
   // 👉 Хэрэглэгчийн мэдээллийг хадгалах код
 
-    const line = users.map((u) => `${u.username}, ${u.pin}, ${u.balance} `); 
+  const line = users.map((u) => `${u.username}, ${u.pin}, ${u.balance} `);
 
-    fs.writeFileSync('users.txt',  line.join("\n"))
-
-   
+  fs.writeFileSync("users.json", line.join("\n"));
 }
 
 // logTransaction(): transactions.txt-д бичих
 function logTransaction(username, type, amount) {
   // 👉 Гүйлгээний лог бичих код
   const log = `${new Date().toLocaleString()} | ${username} | ${type} | ${amount}\n`;
-  fs.appendFileSync("transactions.txt", log);
+  fs.appendFileSync("transactions.json", log);
 }
 
 // =======================
 // Register (шинэ хэрэглэгч)
 // =======================
 function register() {
-  const users= readUsers();
+  const users = readUsers();
   // 👉 Шинэ хэрэглэгчийн нэр асуух
   // 👉 PIN код асуух
   // 👉 Эхний үлдэгдэл асуух
   // 👉 users.txt-д хадгалах
 
-rl.question("username?", (username ) =>{
-
-
-   for(const element of users){
-    if(element.username === username){
-            console.log("burtgeltei username baina");
-            return register();
-      
+  rl.question("username?", (username) => {
+    for (const element of users) {
+      if (element.username === username) {
+        console.log("burtgeltei username baina");
+        return register();
+      }
     }
-
-
-   }
-  // const user = users.find(user => user.username === username)
-  // console.log(user,'user')
+    // const user = users.find(user => user.username === username)
+    // console.log(user,'user')
     // if(user === username){
     //   console.log("burtgeltei username baina")
     //   return;
-   
+
     // }
-  rl.question("password?", (pin)=>{
+    rl.question("password?", (pin) => {
+      rl.question("balance:", (balance) => {
+        const newUser = { username, pin, balance };
+        users.push(newUser);
+        writeUsers(users);
 
-      rl.question("balance:", (balance)=>{
-
-        const newUser = {username, pin, balance};
-          users.push(newUser);
-          writeUsers(users);
-
-
-         login();
-      })
-
-
-
-
-  })
-})
+        login();
+      });
+    });
+  });
 }
 
 // =======================
 // Login + Menu
 // =======================
 function login() {
-    const users = readUsers();
-    rl.question("username?", (username)=> {
+  const users = readUsers();
+  rl.question("username?", (username) => {
+    const user = users.find((user) => user.username === username);
 
-             const user = users.find(user => user.username === username)
+    if (!user) {
+      console.log("hereglegch oldsongui!");
+      return login();
+    }
 
-      if(!user){
-        console.log("hereglegch oldsongui!");
+    rl.question("pin:", (pin) => {
+      if (user.pin.trim() !== pin.trim()) {
+        console.log("Pin buruu baina");
         return login();
       }
-
-     rl.question("pin:", (pin)=>{
-        if(user.pin.trim() !== pin.trim()){
-          console.log("Pin buruu baina");
-          return login();
-        }
-        console.log("amjilttai nevterlee");
-        showMenu(user);
-      })
-    })
+      console.log("amjilttai nevterlee");
+      showMenu(user);
+    });
+  });
 }
 
+// console.log(
+// `==== ATM MENU ====
+//1. Үлдэгдэл шалгах
+//2. Мөнгө нэмэх
+//3. Мөнгө авах
+//4. Гарах`
+// );
 
- // console.log(
-   // `==== ATM MENU ====
-    //1. Үлдэгдэл шалгах
-    //2. Мөнгө нэмэх
-    //3. Мөнгө авах
-    //4. Гарах`
-   // );
-  
-  // 👉 Нэвтрэх нэр асуух
-  // 👉 PIN код асуух
-  // 👉 Хэрэглэгчийн мэдээллийг шалгах
-  // 👉 showMenu дуудаж ажиллуулах
-
+// 👉 Нэвтрэх нэр асуух
+// 👉 PIN код асуух
+// 👉 Хэрэглэгчийн мэдээллийг шалгах
+// 👉 showMenu дуудаж ажиллуулах
 
 function showMenu(user) {
   console.log(`👉 Menu-г харуулах
@@ -139,112 +122,97 @@ function showMenu(user) {
    2. Мөнгө нэмэх
    3. Мөнгө авах
    4. Гарах
-   `)
+   `);
 
-   rl.question("Сонголтоо оруул:", (choice)=>{
-    if(choice === "1"){
+  rl.question("Сонголтоо оруул:", (choice) => {
+    if (choice === "1") {
       console.log(`Таны үлэгдэл: ${user.balance}
         1.Дуусгах
         2.Дахин үйлдэл хийх`);
-        rl.question("Дараагийн үйлдэл:", (inchoice) => {
-          if(inchoice === "1"){
-           return display();
-          } else if(inchoice === "2"){
-           return showMenu(user);
-          }
-        })
-      
-    } else if(choice === "2"){
-      rl.question("Нэмэх дүн:", (amnt)=>{
+      rl.question("Дараагийн үйлдэл:", (inchoice) => {
+        if (inchoice === "1") {
+          return display();
+        } else if (inchoice === "2") {
+          return showMenu(user);
+        }
+      });
+    } else if (choice === "2") {
+      rl.question("Нэмэх дүн:", (amnt) => {
         const amount = parseInt(amnt);
-       console.log(`Таны үлдэгдэл одоо: ${user.balance += amount} боллоо`);
+        console.log(`Таны үлдэгдэл одоо: ${(user.balance += amount)} боллоо`);
         updateUser(user);
         logTransaction(user.username, "deposit", amount);
-        console.log("Amjilttai tseneglelee" 
-        );
+        console.log("Amjilttai tseneglelee");
         console.log(`
           1. Дуусгах
-          2. Дахин үйлдэл хийх`)
-        rl.question("Daraagiin uildel:", (inchoice)=>{
-          if(inchoice==="1"){
+          2. Дахин үйлдэл хийх`);
+        rl.question("Daraagiin uildel:", (inchoice) => {
+          if (inchoice === "1") {
             return display();
-          }else if(inchoice==="2"){
+          } else if (inchoice === "2") {
             return showMenu(user);
           }
-        })
-        
-        
-
+        });
       });
-    } else if(choice === "3"){
+    } else if (choice === "3") {
       rl.question("Татах дүн:", (amnt) => {
         const amount = parseInt(amnt);
-        if (amount>user.balance){
+        if (amount > user.balance) {
           console.log("Үлдэгдэл хүрэлцэхгүй байна");
-
-        }else {
-          console.log(`Таны үлдэгдэл одоо:${user.balance -= amount} боллоо`);
+        } else {
+          console.log(`Таны үлдэгдэл одоо:${(user.balance -= amount)} боллоо`);
           updateUser(user);
-          logTransaction(user.username, "withdraw", amount)
+          logTransaction(user.username, "withdraw", amount);
           console.log("Амжилттай таталт хийлээ");
         }
         console.log(`
         1.Дуусгах
-        2. Дахин үйлдэл хийх`)
-          rl.question("Дараагийн үйлдэл:", (inchoice)=>{
-            if(inchoice==="1"){
-              return display();
-            }else if(inchoice==="2"){
-              return showMenu(user)
-            }
-          })
-      })
-    }
-    else if(choice === "4"){
+        2. Дахин үйлдэл хийх`);
+        rl.question("Дараагийн үйлдэл:", (inchoice) => {
+          if (inchoice === "1") {
+            return display();
+          } else if (inchoice === "2") {
+            return showMenu(user);
+          }
+        });
+      });
+    } else if (choice === "4") {
       console.log("Гарах");
       display();
-    }
-    else {
-      console.log("Буруу сонголт")
+    } else {
+      console.log("Буруу сонголт");
       showMenu(user);
     }
-   })
-
- 
-  
-  
+  });
 }
 
-function updateUser(updatedUser){
+function updateUser(updatedUser) {
   const users = readUsers();
   const newUsers = users.map((u) =>
-  u.username === updatedUser.username ? updatedUser : u);
+    u.username === updatedUser.username ? updatedUser : u
+  );
   writeUsers(newUsers);
 }
 
 // =======================
 // Main
 // =======================
- function display () {console.log(`
+function display() {
+  console.log(`
 ==== ATM SYSTEM ====
 1. Нэвтрэх
-2. Бүртгүүлэх`
-);
+2. Бүртгүүлэх`);
 
-
-rl.question("Сонголтоо оруулна уу: ", (startChoice) => {
-  if (startChoice === "1") {
-    login();
-  } else if (startChoice === "2") {
-    register();
-  } else {
-    console.log("⚠️ Буруу сонголт!");
-    display();  
-    
-  }
-
-
-})
-};
+  rl.question("Сонголтоо оруулна уу: ", (startChoice) => {
+    if (startChoice === "1") {
+      login();
+    } else if (startChoice === "2") {
+      register();
+    } else {
+      console.log("⚠️ Буруу сонголт!");
+      display();
+    }
+  });
+}
 
 display();
