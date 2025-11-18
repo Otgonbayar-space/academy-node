51698218ace3223020f84bcd008d581f58b4d251
BACK-14 <back-14@Erxess-Mac-mini.local>
2025-11-18 17:53:12 +0800

    sfdg

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,14 @@
-export class BankService {
-  checkBalance() {
-    return "6000";
-  }
+async function readUsers() {
+  if (!fs.existsSync("data/users.json")) return [];
+
+  const data = fs.readFileSync("data/users.json", "utf-8");
+  if (!data.trim()) return [];
+  return data.split("\n").map((line) => {
+    const [username, password, balance] = line.split(",");
+    return {
+      username: username.trim(),
+      password: password.trim(),
+      balance: parseInt(balance.trim()),
+    };
+  });
 }
