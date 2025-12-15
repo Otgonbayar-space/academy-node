7d3d39038f591b373d2d79f5bb19c6e7d164e45e
BACK-14 <back-14@Erxess-Mac-mini.local>
2025-12-15 20:22:33 +0800

    tstststststst

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import type { Request, Response } from "express";
 import { userRouters } from "./routers/userRoutes.js";
 import { bankRouters } from "./routers/bankRoutes.js";
-import { connectDb } from "./db.js";
+// import { connectDb } from "./db.js";
 
 const app = express();
 
@@ -10,7 +11,11 @@ app.use(express.json());
 app.use("/user", userRouters);
 app.use("/bank", bankRouters);
 
-await connectDb();
+app.get("/asd", (req: Request<[id: string]>, res: Response) => {
+  const id = req.params.id;
+  res.send(id);
+});
+// await connectDb();
 
 app.listen(3001, () => {
   console.log("express app running at 3001");
