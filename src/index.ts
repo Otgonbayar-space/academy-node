7085fb186c251db4df5644e1cad511703c585b88
BACK-14 <back-14@Erxess-Mac-mini.local>
2026-01-05 20:30:46 +0800

    nasd

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,57 @@
+// import express from "express";
+// import { ApolloServer } from "apollo-server-express";
+// import mongoose from "mongoose";
+// import { typeDefs } from "./graphql/schema.ts";
+// import { resolvers } from "./graphql/resolvers.ts";
+
+// const startServer = async () => {
+//   const app = express();
+
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app: app as any });
+
+//   await mongoose.connect(
+//     "mongodb+srv://kaobuuu30_db_user:figEDNEj8W$tbgy@cluster0.celbvau.mongodb.net/sample_mflix?appName=Cluster0"
+//   );
+//   console.log("MongoDB connected");
+
+//   app.listen({ port: 4000 }, () =>
+//     console.log(`Server running at http://localhost:4000${server.graphqlPath}`)
+//   );
+// };
+
+startServer();
+
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import { movieRouter } from "./movies/router.ts";
+import { typeDefs } from "./graphql/schema.ts";
+import { resolvers } from "./graphql/resolvers.ts";
 
-// Express app
-const app = express();
-app.use(bodyParser.json());
+const startServer = async () => {
+  const app = express();
 
-app.use("/movie", movieRouter);
-
-// MongoDB connection
-mongoose
-  .connect(
-    "mongodb+srv://kaobuuu30_db_user:figEDNEj8W$tbgy@cluster0.celbvau.mongodb.net/sample_mflix?appName=Cluster0"
-  )
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err: Error) => {
-    console.error("MongoDB connection error:", err);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
   });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await mongoose.connect(
+    "mongodb+srv://kaobuuu30_db_user:figEDNEj8W$tbgy@cluster0.celbvau.mongodb.net/sample_mflix?appName=Cluster0"
+  );
+  console.log("MongoDB connected");
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`Server running at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer();
