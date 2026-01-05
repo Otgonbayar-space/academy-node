d774b71c718fd0ca538427e78a922dcda8cfbb71
BACK-14 <back-14@Erxess-Mac-mini.local>
2026-01-05 17:43:57 +0800

    qwer

--- src/movies/controller.ts
+++ src/movies/controller.ts
@@ -1,4 +1,14 @@
 import type { Request, Response } from "express";
 import { Movies } from "./models.ts";
+import mongoose from "mongoose";
 
-const MovieAddController = async (req: Request, res: Response) => {};
+const MovieAddController = async (req: Request, res: Response) => {
+  const movieadd = await Movies.insertMany({
+    title: "Galaxy Quest",
+    year: 2023,
+    genre: "Sci-Fi and Comedy",
+    directors: "Jane Smith",
+    cast: "Actor X and Actor Y",
+    Imdb: 7.8,
+  });
+};
